2680dc5d408e77d3ff6d3a8f5584ab686ac6a11d
lakkyung.jung <lakkyung.jung@samsung.com>
2018-04-16 15:46:16 +0900

    sched/fair: update util_est only on util_avg updates
    The estimated utilization of a task is currently updated every time the
    task is dequeued. However, to keep overheads under control, PELT signals
    are effectively updated at maximum once every 1ms.
    
    Thus, for really short running tasks, it can happen that their util_avg
    value has not been updates since their last enqueue.  If such tasks are
    also frequently running tasks (e.g. the kind of workload generated by
    hackbench) it can also happen that their util_avg is updated only every
    few activations.
    
    This means that updating util_est at every dequeue potentially introduces
    not necessary overheads and it's also conceptually wrong if the util_avg
    signal has never been updated during a task activation.
    
    Let's introduce a throttling mechanism on task's util_est updates
    to sync them with util_avg updates. To make the solution memory
    efficient, both in terms of space and load/store operations, we encode a
    synchronization flag into the LSB of util_est.enqueued.
    This makes util_est an even values only metric, which is still
    considered good enough for its purpose.
    The synchronization bit is (re)set by __update_load_avg_se() once the
    PELT signal of a task has been updated during its last activation.
    
    Such a throttling mechanism allows to keep under control util_est
    overheads in the wakeup hot path, thus making it a suitable mechanism
    which can be enabled also on high-intensity workload systems.
    Thus, this now switches on by default the estimation utilization
    scheduler feature.
    
    Change-Id: Ia548c1fa33ab1e9d20faa0bf7503ebaba5946063
    Suggested-by: Chris Redpath <chris.redpath@arm.com>
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Paul Turner <pjt@google.com>
    Cc: Vincent Guittot <vincent.guittot@linaro.org>
    Cc: Morten Rasmussen <morten.rasmussen@arm.com>
    Cc: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: krazey <admin@krazey.de>
    
    Conflicts:
    kernel/sched/fair.c

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -3110,6 +3110,32 @@ __update_load_avg_blocked_se(u64 now, int cpu, struct sched_entity *se)
 	return ___update_load_avg(now, cpu, &se->avg, 0, 0, NULL, NULL);
 }
 
+/*
+ * When a task is dequeued, its estimated utilization should not be update if
+ * its util_avg has not been updated at least once.
+ * This flag is used to synchronize util_avg updates with util_est updates.
+ * We map this information into the LSB bit of the utilization saved at
+ * dequeue time (i.e. util_est.dequeued).
+ */
+#define UTIL_AVG_UNCHANGED 0x1
+
+static inline void cfs_se_util_change(struct sched_avg *avg)
+{
+	unsigned int enqueued;
+
+	if (!sched_feat(UTIL_EST))
+		return;
+
+	/* Avoid store if the flag has been already set */
+	enqueued = avg->util_est.enqueued;
+	if (!(enqueued & UTIL_AVG_UNCHANGED))
+		return;
+
+	/* Reset flag to report util_avg has been updated */
+	enqueued &= ~UTIL_AVG_UNCHANGED;
+	WRITE_ONCE(avg->util_est.enqueued, enqueued);
+}
+
 static int
 __update_load_avg_se(u64 now, int cpu, struct cfs_rq *cfs_rq, struct sched_entity *se)
 {
