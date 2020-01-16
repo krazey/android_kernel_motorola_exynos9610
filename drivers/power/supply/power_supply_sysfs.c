d62fd2d2e14a1febc1ffec40d2b96064ade70e25
Dan Murphy <dmurphy@ti.com>
2020-01-16 11:50:37 -0600

    FROMLIST: power_supply: Add additional health properties to the header
    Add HEALTH_WARM, HEALTH_COOL and HEALTH_HOT to the health enum.
    
    Bug: 149071038
    Test: Builds
    Link: https://lore.kernel.org/linux-pm/20200116175039.1317-3-dmurphy@ti.com/
    
    Signed-off-by: Dan Murphy <dmurphy@ti.com>
    Tested-by: Guru Das Srinagesh <gurus@codeaurora.org>
    Signed-off-by: Sandeep Patil <sspatil@google.com>
    Change-Id: I5a99577e8c8c38c2ea10a339223c177d18c93d37
    Signed-off-by: krazey <admin@krazey.de>
    
    Conflicts:
    drivers/power/supply/power_supply_sysfs.c
    include/linux/power_supply.h

--- drivers/power/supply/power_supply_sysfs.c
+++ drivers/power/supply/power_supply_sysfs.c
@@ -58,7 +58,8 @@ static const char * const power_supply_charge_type_text[] = {
 static const char * const power_supply_health_text[] = {
 	"Unknown", "Good", "Overheat", "Dead", "Over voltage",
 	"Unspecified failure", "Cold", "Watchdog timer expire",
-	"Safety timer expire", "Under voltage"
+	"Safety timer expire", "Under voltage", "Over current",
+	"Warm", "Cool", "Hot"
 };
 
 static const char * const power_supply_technology_text[] = {
