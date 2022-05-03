495001b5b40caf2c12b0ad6af7b2001e0b9e02f0
krazey <admin@krazey.de>
2022-05-04 00:14:56 +0200

    ANDROID: usb: configfs: fix unused functions for android, 'CONFIG_USB_CONFIGFS_UEVENT'
    Signed-off-by: krazey <admin@krazey.de>

--- drivers/usb/gadget/configfs.c
+++ drivers/usb/gadget/configfs.c
@@ -1523,6 +1523,7 @@ static void configfs_composite_unbind(struct usb_gadget *gadget)
 	spin_unlock_irqrestore(&gi->spinlock, flags);
 }
 
+#ifndef CONFIG_USB_CONFIGFS_F_ACC
 static int configfs_composite_setup(struct usb_gadget *gadget,
 		const struct usb_ctrlrequest *ctrl)
 {
@@ -1569,6 +1570,7 @@ static void configfs_composite_disconnect(struct usb_gadget *gadget)
 	composite_disconnect(gadget);
 	spin_unlock_irqrestore(&gi->spinlock, flags);
 }
+#endif
 
 static void configfs_composite_suspend(struct usb_gadget *gadget)
 {
